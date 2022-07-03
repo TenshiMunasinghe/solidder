6c52e654c288bc911c27ace51bd1c7802287d658
TenshiMunasinghe <shadowghost1007@gmail.com>
2022-07-03 12:18:20 +0900

    enable strict mode for trpc

--- api/trpc/[trpc].ts
+++ api/trpc/[trpc].ts
@@ -1,14 +1,41 @@
 import { PrismaClient } from '@prisma/client'
 import * as trpc from '@trpc/server'
 import * as trpcNext from '@trpc/server/adapters/next'
-
+import z from 'zod'
 const prisma = new PrismaClient()
 
-const appRouter = trpc.router().query('users', {
-  async resolve() {
-    return prisma.user.findMany()
-  },
-})
+const appRouter = trpc
+  .router()
+  .query('login', {
+    input: z.object({
+      email: z.string(),
+      password: z.string(),
+    }),
+    async resolve({ input }) {
+      return prisma.user.findFirst({
+        where: {
+          email: input.email,
+          password: input.password,
+        },
+      })
+    },
+  })
+  .mutation('signup', {
+    input: z.object({
+      email: z.string(),
+      name: z.string(),
+      password: z.string(),
+    }),
+    async resolve({ input }) {
+      return prisma.user.create({
+        data: {
+          email: input.email,
+          name: input.name,
+          password: input.password,
+        },
+      })
+    },
+  })
 
 // export type definition of API
 export type AppRouter = typeof appRouter
--- src/lib/trpc.ts
+++ src/lib/trpc.ts
@@ -1,7 +1,8 @@
-import { createTRPCClient } from '@trpc/client'
+import { createTRPCClient, TRPCRequestOptions } from '@trpc/client'
 import { createResource } from 'solid-js'
 
 import type { inferHandlerInput, inferProcedureOutput } from '@trpc/server'
+
 import type { AppRouter } from '../../api/trpc/[trpc]'
 
 export const trpcClient = createTRPCClient<AppRouter>({ url: '/api/trpc' })
@@ -9,12 +10,14 @@ export const trpcClient = createTRPCClient<AppRouter>({ url: '/api/trpc' })
 type AppQueries = AppRouter['_def']['queries']
 type AppQueryKeys = keyof AppQueries & string
 
+type Log = inferHandlerInput<AppQueries['login']>
+
 export const createTrpcQuery = <TPath extends AppQueryKeys>(
   path: TPath,
-  ...args: inferHandlerInput<AppQueries[TPath]>
+  ...args: [...inferHandlerInput<AppQueries[TPath]>, TRPCRequestOptions?]
 ) => {
   const fetchData = async () => {
-    return trpcClient.query(path, ...(args as any))
+    return trpcClient.query(path, ...args)
   }
 
   return createResource(fetchData)
@@ -26,8 +29,10 @@ type AppMutationKeys = keyof AppMutations & string
 export const createTrpcMutation = <TPath extends AppMutationKeys>(
   path: TPath
 ) => {
-  const fetchData = async (...args: inferHandlerInput<AppMutations[TPath]>) => {
-    return trpcClient.mutation(path, ...(args as any))
+  const fetchData = async (
+    args: [...inferHandlerInput<AppMutations[TPath]>, TRPCRequestOptions?]
+  ) => {
+    return trpcClient.mutation(path, ...args)
   }
 
   return { mutate: fetchData }
