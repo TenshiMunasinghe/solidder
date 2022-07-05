2e297a7622187506ef4f045b6e531ea9c60a856c
TenshiMunasinghe <shadowghost1007@gmail.com>
2022-07-06 00:13:32 +0900

    extend full width

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -5,7 +5,7 @@ interface Props extends JSX.InputHTMLAttributes<HTMLInputElement> {}
 const Input = (props: Props) => {
   return (
     <input
-      class='max-w-sm p-1 bg-neutral-700 focus:outline-none focus:ring-1 focus:ring-accent rounded-sm overflow-hidden'
+      class='max-w-sm w-full p-1 bg-neutral-700 focus:outline-none focus:ring-1 focus:ring-accent rounded-sm overflow-hidden'
       {...props}
     />
   )
